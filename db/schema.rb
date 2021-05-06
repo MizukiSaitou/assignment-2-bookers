ebd267860b3a763760dfff3489756eef6043847a
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-06 13:30:30 +0000

    [Add]3-8 編集・削除機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_062753) do
+ActiveRecord::Schema.define(version: 2021_05_05_080542) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
