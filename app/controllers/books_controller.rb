7cb4d4673a1ee826eca7ce9f97b4343802ff0e2f
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-08 14:20:49 +0000

    [Add]3-8 Show,Edit,Destroyのリンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,23 +19,26 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
-    book = Book.find(params[:id])  
-    book.destroy  
-    redirect_to books_path  
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
+   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
+   end
+   
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
+  
 end
