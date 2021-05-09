b67df446e22ac8f296407a641f84c6a0555ec92a
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-09 06:08:36 +0000

    [Add]3-8バンデーション機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -16,9 +17,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
     flash[:notice] = "Book was successfully updated."
   end
 
@@ -29,16 +33,20 @@ class BooksController < ApplicationController
   end
 
    def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
     flash[:notice] = "Book was successfully created."
    end
-   
+
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
-  
+
 end
